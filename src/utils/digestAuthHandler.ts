925ce09a35436b673421ba79613b662d53dbb36f
NanasoDev <mariano.ceballos@gmail.com>
2023-09-13 18:58:29 -0300

    Add: interface and add filters to deviceApi

--- src/utils/digestAuthHandler.ts
+++ src/utils/digestAuthHandler.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import crypto from "crypto";
 require('dotenv').config()
 import saveInLogFile from "./saveInLogFile";
+import { DeviceData } from "../interfaces/DeviceData.interface";
 
 
 // Configuración de la solicitud
@@ -11,25 +12,34 @@ const username = process.env.DEVICE_1_ADMIN_USERNAME;
 const password = process.env.DEVICE_1_ADMIN_PASSWORD;
 
 
-export const getDataFromDevice = async (): Promise<{} | undefined> => {
+export const getDataFromDevice = async ({ 
+      searchResultPosition = 0,
+      maxResults = 30,
+      beginSerialNo = 1,
+      endSerialNo = 564
+}): Promise<DeviceData | undefined> => {
 
-  let customHeaders = '';
-  let resp = {};
   
+  let customHeaders = '';
   // Paso 1: Realiza una solicitud GET para obtener los parámetros de autenticación digest
-  await axios.get( url )
-    .then ( response => { console.log( 'RESPONSE: ', response );})
-    .catch( error    => { 
-      // console.log(error.response.headers);
-      customHeaders = error.response?.headers['www-authenticate']; 
-    }
-  );
+  try {
+    const response = await axios.get( url );
+    console.log( 'RESPONSE: ', response );
+  } catch ( error: any ) {
+    // console.log( error );
+    customHeaders = error.response?.headers['www-authenticate']; 
+  }
+
+  if ( customHeaders === undefined ) {
+    saveInLogFile( new Date().toLocaleString() + ' - No se pudo obtener el encabezado WWW-Authenticate' );
+    return;
+  }
 
   const authHeader = customHeaders ;
   const realmMatch = authHeader.match(/realm="([^"]+)"/);
   const nonceMatch = authHeader.match(/nonce="([^"]+)"/);
 
-  if (realmMatch && nonceMatch) {
+  if ( realmMatch && nonceMatch ) {
     const realm = realmMatch[1];
     const nonce = nonceMatch[1];
 
@@ -45,7 +55,7 @@ export const getDataFromDevice = async (): Promise<{} | undefined> => {
     const cnonce = crypto.randomBytes(16).toString('hex');
 
     // Paso 5: Calcula el hash MD5 del método HTTP y la URL
-    const httpMethod = 'POST'; // Puedes cambiarlo según tu solicitud
+    const httpMethod = 'POST';
     const ha2 = crypto.createHash('md5')
       .update(`${httpMethod}:${uri}`)
       .digest('hex');
@@ -60,8 +70,6 @@ export const getDataFromDevice = async (): Promise<{} | undefined> => {
     const authHeaderDigest = `Digest username="${username}", realm="${realm}", nonce="${nonce}", uri="${uri}", qop=${qop}, nc=${nc}, cnonce="${cnonce}", response="${response}"`;
 
     // Paso 8: Realiza la solicitud real con el encabezado de autenticación digest
-    
-    
     try {
       const { data } = await axios({
       method: httpMethod,
@@ -74,11 +82,13 @@ export const getDataFromDevice = async (): Promise<{} | undefined> => {
       data: JSON.stringify({
         "AcsEventCond": {
           "major": 5,
-          "maxResults": 100,
+          "maxResults": maxResults,
           "minor": 75,
           "searchID": "1",
-          "searchResultPosition": 0,
-          "timeReverseOrder": true
+          "searchResultPosition": searchResultPosition,
+          "timeReverseOrder": true,
+          "beginSerialNo": beginSerialNo, 
+          "endSerialNo": endSerialNo
         }})
       });
       
@@ -88,16 +98,6 @@ export const getDataFromDevice = async (): Promise<{} | undefined> => {
     } catch (error) {
       saveInLogFile( new Date().toLocaleString() + ' - Error en consulta a dispositivo: ' + error );
     }
-    
-    // .then( response => {
-    //     // Manejar la respuesta del servidor aquí
-    //     resp = response.data;
-    //   })
-    //   .catch( error => {
-        
-    //   });
-
-    //   return resp;
   } else {
     saveInLogFile( new Date().toLocaleString() + ' - No se pudo obtener el encabezado WWW-Authenticate' );
   }
