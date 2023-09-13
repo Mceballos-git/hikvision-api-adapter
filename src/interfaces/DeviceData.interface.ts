5d4e7f0c5af0a0fc1268a30f26e57ee5bb395274
NanasoDev <mariano.ceballos@gmail.com>
2023-09-13 18:56:48 -0300

    Add: DeviceDataInterface

---
+++ src/interfaces/DeviceData.interface.ts
@@ -0,0 +1,27 @@
+export interface DeviceData {
+    AcsEvent: {
+      searchID: string,
+      totalMatches: number,
+      responseStatusStrg: string,
+      numOfMatches: number,
+      InfoList: 
+        [ EventData ]
+    }                                   
+}
+
+export interface EventData {
+  major: number,
+  minor: number,
+  time: string,
+  cardType: number,
+  name: string,
+  cardReaderNo: number,
+  doorNo: number,
+  employeeNoString: string,
+  type: number,
+  serialNo: number,
+  userType: string,
+  currentVerifyMode: string,
+  mask: string,
+  pictureURL: string
+}
