d9e5d010799c6534c1eec2997a5db7ba30474f72
NanasoDev <mariano.ceballos@gmail.com>
2023-09-27 16:18:46 -0300

    Fix: Refactor index

--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,18 @@
-import getDataFromDevice, { getImageFromUrl } from "./utils/digestAuthHandler";
-import { LastEventNumber, getLastEventfromDB, initDB, insertDataOnDB } from "./utils/dbUtils";
-import { DeviceData, EventData, NewEventData } from './interfaces/DeviceData.interface';
-import { saveYellowInLogFile } from "./utils/saveInLogFile";
-
+import { backupLargeFiles } from "./utils/backupLargeFiles";
+import { initDB } from "./utils/dbUtils";
+import { getDataAndSaveInDB } from "./utils/getDataAndSaveInDB";
 var cron = require('node-cron');
 
 
-initDB();
-// startServer();
-
-   
-async function getData() {
-
-  try {
-    let eventDataArray: EventData[];
-
-    // Extraigo el registro con ID mas alto de la DB
-    let lastEventFromDB: LastEventNumber | null = await getLastEventfromDB();
-    if ( !lastEventFromDB ) {
-      lastEventFromDB = { serialNo: 1 };
-    };
-
-    // Realizo la llamada al dispositivo 
-    // Buscando eventos con ID mas alto que el de la DB
-    const deviceData: DeviceData | undefined = await getDataFromDevice({
-      searchResultPosition: 0,
-      maxResults: 15,
-      beginSerialNo: lastEventFromDB.serialNo + 1,
-      endSerialNo: lastEventFromDB.serialNo + 100,
-    });
-
-
-    if ( deviceData?.AcsEvent?.totalMatches ) { 
-
-      // Extraigo el array de eventos del dispositivo
-      eventDataArray = deviceData?.AcsEvent.InfoList;
-
-      for ( const event of eventDataArray ) {
-        const pictureURL = event.pictureURL;
-
-        // Convierto a BLOB e inserto el evento en la DB
-        try {
-          const pictureData: Buffer | undefined = await getImageFromUrl( pictureURL );
-
-          if (pictureData) {
-
-            let newEvent: NewEventData = {
-              major: event.major,
-              minor: event.minor,
-              time: event.time,
-              cardType: event.cardType,
-              name: event.name,
-              cardReaderNo: event.cardReaderNo,
-              doorNo: event.doorNo,
-              employeeNoString: event.employeeNoString,
-              type: event.type,
-              serialNo: event.serialNo,
-              userType: event.userType,
-              currentVerifyMode: event.currentVerifyMode,
-              mask: event.mask,
-              pictureBlob: pictureData
-            }
-
-            await insertDataOnDB(newEvent);
-          }
-        } catch (error) {
-          saveYellowInLogFile( `Error al obtener la imagen` + error);
-        }
-      }
-    }
-  } catch( error ) {
-    saveYellowInLogFile( `Error de primer metodo: ` + error );
-  };
-}
-
-
 // Programa la tarea para ejecutarse cada un minuto
-
-let tiempo = process.env.TIMER_ACCESO_DISPOSITIVO;
-//"$tiempo"
-
-const tareaCron = cron.schedule( process.env.TIMER_ACCESO_DISPOSITIVO, () => {
+const tareaCron = cron.schedule( process.env.TIMER_ACCESO_DISPOSITIVO, async () => {
   try {
-    getData(); //lectura de datos del lector y graba en sqlite
+
+    await initDB();  
+    await getDataAndSaveInDB(); //lectura de datos del lector y graba en sqlite
+    await backupLargeFiles();
 
     // TODO  postDataToCheckpint()  //envia a api de chackpoint
-
-    //TODO purga o verificar fecha para hacer purga
-
-
-
-
 
   } catch (error) {
     console.error('Error en la tarea:', error);
@@ -112,4 +34,8 @@ process.on('unhandledRejection', (reason, promise) => {
   // Vuelve a iniciar la tarea después de un error de rechazo
   tareaCron.start();
 });
+
+
+ 
+
 
