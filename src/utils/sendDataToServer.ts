23a60b40ca5928782b68056c3d4bd46413cc35c1
NanasoDev <mariano.ceballos@gmail.com>
2023-09-30 19:51:27 -0300

    Add: sendDataToServer improvements

--- src/utils/sendDataToServer.ts
+++ src/utils/sendDataToServer.ts
@@ -1,14 +1,18 @@
 import { DatabaseEventData } from "../interfaces/DatabaseEventData.interface";
-import { retrieveDatabaseUnsubmittedRecords } from "./dbUtils"
+import { markRecordAsSent, retrieveDatabaseUnsubmittedRecords } from "./dbUtils"
 import { getBase64ImageFromUrl } from "./digestAuthHandler";
 import { CheckpointEventData } from '../interfaces/CheckpointEventData.interface';
 import axios from "axios";
+import { cleanTimeZoneFromFullDate, extractDateFromFullDate, extractTimeFromFullDate } from "./formatDate";
+import { saveGreenInLogFile, saveYellowInLogFile } from "./saveInLogFile";
 
 
 
 const checkpointURL = process.env.CHECKPOINT_SERVER_URL as string;
+const numeroEmpresa = process.env.NUMERO_EMPRESA;
+const numeroSucursal = process.env.NUMERO_SUCURSAL;
 
-export const sendDataToServer = async (): Promise<any> => {
+export const sendDataToServer = async (): Promise<void> => {
   return new Promise( async (resolve, reject) => {
     try {
       // Consulto los registros en DB que NO estan enviados
@@ -16,41 +20,38 @@ export const sendDataToServer = async (): Promise<any> => {
 
       // Creo el array de eventos que voy a mandar
       let dataToSend: CheckpointEventData[] = [];
-      
-      for ( const event of unsubmittedRecords ) {
 
-        const pictureData: string | undefined = await getBase64ImageFromUrl( event.pictureURL );
+      if ( unsubmittedRecords.length > 0 ) {
 
-        let eventToSend: CheckpointEventData = {
-          time: event.time,
-          numero_empresa: event.numero_empresa,
-          numero_sucursal: event.numero_sucursal,
-          pictureBase64: pictureData!,
-          name: event.name,
-          employeeNoString: event.employeeNoString,
+        for await ( const event of unsubmittedRecords ) {
 
-          // major: 0,
-          // minor: 0,
-          // cardType: 0,
-          // cardReaderNo: 0,
-          // doorNo: 0,
-          // type: 0,
-          // serialNo: 0,
-          // userType: "",
-          // currentVerifyMode: "",
-          // mask: "",
-          // enviado: false
-        };
+          const pictureData: string | undefined = await getBase64ImageFromUrl( event.pictureURL );
 
-        dataToSend.push(eventToSend);
+          let eventToSend: CheckpointEventData = {
+            id: event.serialNo,
+            empleado: parseInt(event.employeeNoString),
+            hora: cleanTimeZoneFromFullDate( event.time )!,
+            hora1: extractTimeFromFullDate( event.time )!,
+            fecha: extractDateFromFullDate( event.time )!,
+            empresa: parseInt( numeroEmpresa! ),
+            sucursal: parseInt( numeroSucursal! ),
+            imagen: pictureData!
+          };
+
+          dataToSend.push( eventToSend );
+        }
+        const response = await axios.post( checkpointURL, dataToSend );
+        console.log(response);
+
+        for ( const event of unsubmittedRecords ) {
+          markRecordAsSent( event.serialNo );
+        }
+        resolve(saveGreenInLogFile( `${dataToSend.length} registros enviados correctamente` ));
+      } else {
+        resolve(saveGreenInLogFile( `Sin registros nuevos para enviar` ));
       }
-
-      axios.post( checkpointURL, dataToSend );
-
-      resolve(console.log(dataToSend));
-      
     } catch (error) {
-      
+      reject( saveYellowInLogFile( 'No se pudieron enviar los registros: ' + error))
     }
     
 
