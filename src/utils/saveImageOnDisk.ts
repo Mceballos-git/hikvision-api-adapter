aa4f29a451bfc1c87cfe09873ad5f82dc7a2f73d
NanasoDev <mariano.ceballos@gmail.com>
2023-10-18 23:38:06 -0300

    Add: tryCatch on saveImageOnDisk

--- src/utils/saveImageOnDisk.ts
+++ src/utils/saveImageOnDisk.ts
@@ -1,10 +1,14 @@
-import { saveInLogFile } from "./saveInLogFile";
+import { saveInLogFile, saveYellowInLogFile } from "./saveInLogFile";
 
 const fs = require('fs');
 
 
-export const saveImageOnDisk = ( serialNo: number, buffer: any) => {
+export const saveImageOnDisk = ( serialNo: number, buffer: any ) => {
   const outputPath = "./images/imagen"; // Ruta donde deseas guardar el archivo
-  fs.writeFileSync( outputPath + serialNo +'.jpg', buffer );
-  saveInLogFile(`Imagen descargada y guardada en ${outputPath} ${serialNo}`);
-}
+  try {
+    fs.writeFileSync( outputPath + serialNo +'.jpg', buffer );
+    saveInLogFile(`Imagen descargada y guardada en ${outputPath} ${serialNo}`);
+  } catch (error) {
+    saveYellowInLogFile(`Error al guardar imagen${serialNo} en disco`);
+  };
+};
